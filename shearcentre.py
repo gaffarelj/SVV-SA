51087da728305d491436fe6f7a0d9354a8910255
afonsotuna <afonso.tuna@gmail.com>
2020-02-19 13:10:10 +0100

    Debugging, shear flows are now correct per section

--- shearcentre.py
+++ shearcentre.py
@@ -37,13 +37,13 @@ def qb_3(s3):  # 0 to 0.4068
     boom9_s3 = -boomcoords_hinge[8, 0] / math.cos(beta)
     boomspace_s3 = (boomcoords_hinge[8, 0] - boomcoords_hinge[9, 0]) / math.cos(beta)
     if s3 < boom9_s3:
-        qb = c * tskin * integrate(lambda x: r - x, 0, s3) + qb_1(math.pi/2) + qb_2(0.0865)
+        qb = c * tskin * integrate(lambda x: r - x * math.sin(beta), 0, s3) + qb_1(math.pi/2) + qb_2(0.0865)
     else:
-        nbooms = int((s3 - boom9_s3) / boomspace_s3)
+        nbooms = int((s3 - boom9_s3) / boomspace_s3)+1
         qb_booms = 0
         for i in range(8, 8 + nbooms):
             qb_booms += stiff_area * boomcoords_hinge[i, 1]
-        qb_skin = tskin * integrate(lambda x: r - x, 0, s3)
+        qb_skin = tskin * integrate(lambda x: r - x * math.sin(beta), 0, s3)
         qb = c * (qb_skin + qb_booms) + qb_1(math.pi/2) + qb_2(0.0865)
     return qb
 
@@ -52,11 +52,11 @@ def qb_3(s3):  # 0 to 0.4068
 def qb_4(s4):  # 0 to 0.4068
     c = -1 / Izz
     boom1_s4 = (Ca-r+boomcoords_hinge[0, 0]) / math.cos(beta)
-    boomspace_s4 = (boomcoords_hinge[0, 0] - boomcoords_hinge[1, 0]) / math.cos(beta)
+    boomspace_s4 = -(boomcoords_hinge[0, 0] - boomcoords_hinge[1, 0]) / math.cos(beta)
     if s4 < boom1_s4:
         qb = c * integrate(lambda x: -tskin * math.sin(beta) * x, 0, s4) + qb_3(0.4068)
     else:
-        nbooms = int((s4 - boom1_s4) / boomspace_s4)
+        nbooms = int((s4 - boom1_s4) / boomspace_s4)+1
         qb_booms = 0
         for i in range(0, 0 + nbooms):
             qb_booms += stiff_area * boomcoords_hinge[i, 1]
@@ -73,11 +73,11 @@ def qb_5(s5):  # 0 to 0.0865
 
 
 # SECTION 6
-def qb_6(theta):
+def qb_6(theta):  # -pi/2 to 0
     c = -1 / Izz
     boom6_theta = math.asin(boomcoords[5, 1] / r)
     if theta < boom6_theta:
-        qb = c * (tskin * r ** 2) * integrate(math.sin, theta, 0) + qb_5(0.0865) + qb_4(0.4068)
+        qb = c * (tskin * r ** 2) * integrate(math.sin, math.pi/2, theta) + qb_5(0.0865) + qb_4(0.4068)
     else:
-        qb = c * ((tskin * r ** 2) * integrate(math.sin, theta, 0) + stiff_area * boomcoords[5, 1]) + qb_5(0.0865) + qb_4(0.4068)
+        qb = c * ((tskin * r ** 2) * integrate(math.sin, -math.pi/2, theta) + stiff_area * boomcoords[5, 1]) + qb_5(0.0865) + qb_4(0.4068)
     return qb

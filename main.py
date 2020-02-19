e7620843a21d9cc41d8a20d4638fc361602cfbe4
jgaffarel <j.y.p.gaffarel@student.tudelft.nl>
2020-02-19 10:57:07 +0100

    Added stiff_area as output of the sectionproperties module

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sectionproperties as SP
 
 
-Am, z_centroid, Iyy, Izz, tskin, boomcoords, boomcoords_hinge = SP.get_geometry()
-print(Am, z_centroid, Iyy, Izz, tskin, boomcoords)
+Am, z_centroid, Iyy, Izz, tskin, boomcoords, boomcoords_hinge, stiff_area = SP.get_geometry()
+print(Am, z_centroid, Iyy, Izz, tskin, stiff_area, boomcoords)

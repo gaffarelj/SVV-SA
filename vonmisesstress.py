29cd42138e56d0c17daec76acf5104abfeae2df4
shineminan <m.shi@student.tudelft.nl>
2020-02-19 10:34:53 +0100

    files added

--- vonmisesstress.py
+++ vonmisesstress.py
@@ -1,1 +1,11 @@
-# test
+import math
+
+def vmstress(shearstress,mz,my):
+	"""
+	Calculate the Von Mises stress with formula which can be simplified as:
+	sigma_vm = sqrt(sigma_xx^2 + 3*tau_yz^2)
+	Inputs:
+	- shearstress:
+	- mz:
+	- my:
+	"""

0d6b397814cf73aa14c65519485a26c0d3a8a3d1
jgaffarel <j.y.p.gaffarel@student.tudelft.nl>
2020-02-19 09:14:08 +0100

    Changed input of integrate function

--- integration.py
+++ integration.py
@@ -1,12 +1,17 @@
 import numpy as np
 
-def integrate(f, xs, a, b, threshold=1e-6):
+
+def integrate(f, a, b, threshold=1e-6):
 	# TO CHANGE
 	n = 1e4
+	xs = np.arange(a, b, 1/n)
 	y = [f(x) for x in xs]
 	return np.trapz(y, xs)
 	# TO CHANGE
-
-
-integ = integrate(np.sin, np.arange(0, np.pi/2, 1/1e4), 0, np.pi/2)
-print(integ)
+	
+def integrate_b(f, a, b, threshold=1e-6):
+	pass
+	
+integ = integrate(np.sin, 0, np.pi/2)
+integ_b = integrate_b(np.sin, 0, np.pi/2)
+print(integ, integ_b)

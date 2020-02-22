55b92b97c28ebd34f9302f4f4de5ccc4b5ea7e00
jgaffarel <j.y.p.gaffarel@student.tudelft.nl>
2020-02-22 14:17:29 +0100

    Cleaned sectionproperties, and started shearcenter.

--- sectionproperties.py
+++ sectionproperties.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 """
 Section Properties - idealized aileron
@@ -9,70 +8,87 @@ Stiffener positions are hardcoded (frame of reference is the hinge line), more c
 Inputs are below, as given in Aircraft Data, coded for CRJ700.
 """
 
-def get_geometry(Nstiffeners=13, Ha=0.173, Ca=0.484, tskin=0.0011, tspar=0.0025,
-                hstiff=0.014, tstiff=0.0012, wstiff=0.018, beta=math.atan(8.65/39.75)):
-    boomcoords = get_boomcoords()
-    boomcoords_hinge = boomcoords
-    # stiffener area (in meters^2)
-    stiff_area = (hstiff + wstiff) * tstiff     # small angle approximation is used
-    # enclosed area (in meters^2)
-    Am = math.pi * (Ha / 2) ** 2 + 0.3975 * 0.0865
-    z_centroid, boomcoords = comp_centroid(Ha, tskin, tspar, boomcoords, stiff_area)
-    # moments of inertia (Iyy and Izz about the centroid, in meters^4)
-    Iyy = comp_Iyy(tskin, beta, z_centroid, tspar, Ha, boomcoords, stiff_area)
-    Izz = comp_Izz(tskin, beta, tspar, Ha, boomcoords, stiff_area)
-    return Am, z_centroid, Iyy, Izz, tskin, boomcoords, boomcoords_hinge, stiff_area
+class section():
+    def __init__(self, Nstiffeners=13, Ha=0.173, Ca=0.484, tskin=0.0011, tspar=0.0025,
+                    hstiff=0.014, tstiff=0.0012, wstiff=0.018):
+        self.Nstiffeners = Nstiffeners  # This is actually hard coded by get_boomcoords
+        self.Ha = Ha
+        self.Ca = Ca
+        self.tskin = tskin
+        self.tspar = tspar
+        self.hstiff = hstiff
+        self.tstiff = tstiff
+        self.wstiff = wstiff
+        self.r = Ha/2
+        self.l_spar_to_end = Ca - self.r
+        self.beta = np.arctan(self.r / self.l_spar_to_end)
+        self.l_topskin = self.l_spar_to_end / np.cos(self.beta)
+        # Placeholder vars, to be computed
+        self.stiff_area, self.Iyy, self.Izz, self.boomcoords, self.boomcoords_hinge, \
+            self.Am, self.z_centroid = 0, 0, 0, [], [], 0, 0
+        self.get_boomcoords()
+        self.boomcoords_hinge = self.boomcoords
+        # stiffener area (in meters^2)
+        self.stiff_area = (self.hstiff + self.wstiff) * self.tstiff     # small angle approximation is used
+        # enclosed area (in meters^2)
+        self.Am = np.pi * self.r ** 2 + self.l_spar_to_end * self.r
+        self.comp_centroid()
+        # moments of inertia (Iyy and Izz about the centroid, in meters^4)
+        self.comp_Iyy()
+        self.comp_Izz()
 
-def get_boomcoords():
-    # stiffener positions (origin at hinge line, from bottom right going clockwise, in meters)
-    # [z, y]
-    zy1 = [-0.35671, -0.00888]
-    zy2 = [-0.27513, -0.02663]
-    zy3 = [-0.19355, -0.04438]
-    zy4 = [-0.11197, -0.06213]
-    zy5 = [-0.03039, -0.07989]
-    zy6 = [0.04924, -0.07112]
-    zy7 = [0.0865, 0]
-    zy8 = [0.04924, 0.07112]
-    zy9 = [-0.03039, 0.07989]
-    zy10 = [-0.11197, 0.06213]
-    zy11 = [-0.19355, 0.0444]
-    zy12 = [-0.27513, 0.02663]
-    zy13 = [-0.35671, 0.00888]
-    return np.array([zy1, zy2, zy3, zy4, zy5, zy6, zy7, zy8, zy9, zy10, zy11, zy12, zy13])
+    def get_boomcoords(self):
+        # stiffener positions (origin at hinge line, from bottom right going clockwise, in meters)
+        # [z, y]
+        zy1 = [-0.35671, -0.00888]
+        zy2 = [-0.27513, -0.02663]
+        zy3 = [-0.19355, -0.04438]
+        zy4 = [-0.11197, -0.06213]
+        zy5 = [-0.03039, -0.07989]
+        zy6 = [0.04924, -0.07112]
+        zy7 = [0.0865, 0]
+        zy8 = [0.04924, 0.07112]
+        zy9 = [-0.03039, 0.07989]
+        zy10 = [-0.11197, 0.06213]
+        zy11 = [-0.19355, 0.0444]
+        zy12 = [-0.27513, 0.02663]
+        zy13 = [-0.35671, 0.00888]
+        self.boomcoords = np.array([zy1, zy2, zy3, zy4, zy5, zy6, zy7, zy8, zy9, zy10, zy11, zy12, zy13])
 
-def comp_centroid(Ha, tskin, tspar, boomcoords, stiff_area):
-    # centroid (z-coordinate from hinge line, in meters)
-    sumA_skin = (0.5 * math.pi * Ha * tskin) + 2 * (0.4068 * tskin) + (tspar * Ha)
-    sumAz_skin = (0.5 * math.pi * Ha * tskin) * (Ha / math.pi) + 2 * (0.4068 * tskin) * (-0.19875)
-    sumAz_boom = 0
-    for i in range(len(boomcoords)):
-        sumAz_boom += boomcoords[i, 0] * stiff_area
-    z_centroid = (sumAz_skin + sumAz_boom) / (sumA_skin + 13 * stiff_area)
-    boomcoords_centroid = np.zeros((13, 2))
-    for i in range(len(boomcoords)):        # shift boom origin from hinge line to centroid
-        boomcoords_centroid[i, 0] = boomcoords[i, 0] - z_centroid
-        boomcoords_centroid[i, 1] = boomcoords[i, 1]
-    return z_centroid, boomcoords_centroid
+    def comp_centroid(self):
+        # centroid (z-coordinate from hinge line, in meters)
+        sumA_skin = (0.5 * np.pi * self.Ha * self.tskin) + 2 * (self.l_topskin * self.tskin) + (self.tspar * self.Ha)
+        sumAz_skin = (0.5 * np.pi * self.Ha * self.tskin) * (self.Ha / np.pi) + \
+            2 * (self.l_topskin * self.tskin) * (-self.l_spar_to_end/2)
+        sumAz_boom = 0
+        for i in range(len(self.boomcoords)):
+            sumAz_boom += self.boomcoords[i, 0] * self.stiff_area
+        self.z_centroid = (sumAz_skin + sumAz_boom) / (sumA_skin + len(self.boomcoords) * self.stiff_area)
+        boomcoords_centroid = np.zeros((len(self.boomcoords), 2))
+        for i in range(len(self.boomcoords)):        # shift boom origin from hinge line to centroid
+            boomcoords_centroid[i, 0] = self.boomcoords[i, 0] - self.z_centroid
+            boomcoords_centroid[i, 1] = self.boomcoords[i, 1]
+        self.boomcoords = boomcoords_centroid
 
-def comp_Iyy(tskin, beta, z_centroid, tspar, Ha, boomcoords, stiff_area):
-    # Iyy
-    Iyy_thin = ((tskin * (0.4068 ** 3) * ((math.cos(beta)) ** 2)) / 12) + tskin * 0.4068 * (0.19875 + z_centroid) ** 2
-    Iyy_spar = (Ha * (tspar ** 3)) / 12 + tspar * Ha * z_centroid ** 2
-    Iyy_ring = tskin * (Ha / 2) ** 3 * ((math.pi ** 2 - 8) / (2 * math.pi)) + (0.5 * math.pi * Ha * tskin) * ((Ha / math.pi) - z_centroid) ** 2
-    Iyy_booms = 0
-    for i in range(len(boomcoords)):
-        Iyy_booms += ((boomcoords[i, 0]) ** 2) * stiff_area
-    Iyy_total = 2 * Iyy_thin + Iyy_spar + Iyy_ring + Iyy_booms
-    return Iyy_total
+    def comp_Iyy(self):
+        # Iyy
+        Iyy_thin = self.tskin * self.l_topskin ** 3 * (np.cos(self.beta)) ** 2 / 12 + \
+            self.tskin * self.l_topskin * (self.l_spar_to_end/2 + self.z_centroid) ** 2
+        Iyy_spar = self.Ha * self.tspar ** 3 / 12 + self.tspar * self.Ha * self.z_centroid ** 2
+        Iyy_ring = self.tskin * self.r ** 3 * (np.pi ** 2 - 8) / (2 * np.pi) + \
+            0.5 * np.pi * self.Ha * self.tskin * (self.Ha / np.pi - self.z_centroid) ** 2
+        Iyy_booms = 0
+        for i in range(len(self.boomcoords)):
+            Iyy_booms += ((self.boomcoords[i, 0]) ** 2) * self.stiff_area
+        self.Iyy = 2 * Iyy_thin + Iyy_spar + Iyy_ring + Iyy_booms
 
-def comp_Izz(tskin, beta, tspar, Ha, boomcoords, stiff_area):
-    # Izz
-    Izz_thin = ((tskin * (0.4068 ** 3) * ((math.sin(beta)) ** 2)) / 12) + tskin * 0.4068 * (0.19875 * math.sin(beta)) ** 2
-    Izz_spar = (tspar * (Ha ** 3)) / 12 # This is correct
-    Izz_ring = ((Ha / 2) ** 3 * tskin * math.pi) / 2    # This is correct
-    Izz_booms = 0
-    for i in range(len(boomcoords)):
-        Izz_booms += (boomcoords[i, 1]) ** 2 * stiff_area
-    Izz_total = 2 * Izz_thin + Izz_spar + Izz_ring + Izz_booms
-    return Izz_total
+    def comp_Izz(self):
+        # Izz
+        Izz_thin = self.tskin * self.l_topskin ** 3 * np.sin(self.beta) ** 2 / 12 + \
+            self.tskin * self.l_topskin * (self.l_topskin/2 * np.sin(self.beta)) ** 2
+        Izz_spar = self.tspar * self.Ha ** 3 / 12
+        Izz_ring = self.r ** 3 * self.tskin * np.pi / 2
+        Izz_booms = 0
+        for i in range(len(self.boomcoords)):
+            Izz_booms += (self.boomcoords[i, 1]) ** 2 * self.stiff_area
+        self.Izz = 2 * Izz_thin + Izz_spar + Izz_ring + Izz_booms
--- shearcentre.py
+++ shearcentre.py
@@ -1,87 +1,78 @@
 from integration import integrate
 import math
-import sectionproperties as sp
+import sectionproperties as SP
 import numpy as np
 
-Am, z_centroid, Iyy, Izz, tskin, boomcoords, boomcoords_hinge, stiff_area = sp.get_geometry()
-Ha = 0.173
-Ca = 0.484
-tspar = 0.0025
-hstiff = 0.014
-tstiff = 0.0012
-wstiff = 0.018
-beta = math.atan(8.65 / 39.75)
-l_topskin = 0.3975 / math.cos(beta)
-r = Ha / 2
+sect= SP.section(Ha=0.173, Ca=0.484, tskin=0.0011, tspar=0.0025, hstiff=0.014, tstiff=0.0012, wstiff=0.018)
 
 
 # SECTION 1
 def qb_1(theta):  # 0 to pi/2
-    c = -1 / Izz
-    boom8_theta = math.asin(boomcoords[7, 1] / r)
+    c = -1 / sect.Izz
+    boom8_theta = math.asin(sect.boomcoords[7, 1] / sect.r)
     if theta < boom8_theta:
-        qb = c * (tskin * r ** 2) * integrate(math.sin, 0, theta)
+        qb = c * (sect.tskin * sect.r ** 2) * integrate(math.sin, 0, theta)
     else:
-        qb = c * ((tskin * r ** 2) * integrate(math.sin, 0, theta) + stiff_area * boomcoords[7, 1])
+        qb = c * ((sect.tskin * sect.r ** 2) * integrate(math.sin, 0, theta) + sect.stiff_area * sect.boomcoords[7, 1])
     return qb
 
 
 # SECTION 2
 def qb_2(s2):  # 0 to 0.0865
-    c = -1 / Izz
-    qb = c * integrate(lambda x: tspar * x, 0, s2)
+    c = -1 / sect.Izz
+    qb = c * integrate(lambda x: sect.tspar * x, 0, s2)
     return qb
 
 
 # SECTION 3
 def qb_3(s3):  # 0 to 0.4068
-    c = -1 / Izz
-    boom9_s3 = -boomcoords_hinge[8, 0] / math.cos(beta)
-    boomspace_s3 = (boomcoords_hinge[8, 0] - boomcoords_hinge[9, 0]) / math.cos(beta)
+    c = -1 / sect.Izz
+    boom9_s3 = -sect.boomcoords_hinge[8, 0] / math.cos(sect.beta)
+    boomspace_s3 = (sect.boomcoords_hinge[8, 0] - sect.boomcoords_hinge[9, 0]) / math.cos(sect.beta)
     if s3 < boom9_s3:
-        qb = c * tskin * integrate(lambda x: r - x * math.sin(beta), 0, s3) + qb_1(math.pi / 2) + qb_2(0.0865)
+        qb = c * sect.tskin * integrate(lambda x: sect.r - x * math.sin(sect.beta), 0, s3) + qb_1(math.pi / 2) + qb_2(0.0865)
     else:
         nbooms = int((s3 - boom9_s3) / boomspace_s3) + 1
         qb_booms = 0
         for i in range(8, 8 + nbooms):
-            qb_booms += stiff_area * boomcoords_hinge[i, 1]
-        qb_skin = tskin * integrate(lambda x: r - x * math.sin(beta), 0, s3)
+            qb_booms += sect.stiff_area * sect.boomcoords_hinge[i, 1]
+        qb_skin = sect.tskin * integrate(lambda x: sect.r - x * math.sin(sect.beta), 0, s3)
         qb = c * (qb_skin + qb_booms) + qb_1(math.pi / 2) + qb_2(0.0865)
     return qb
 
 
 # SECTION 4
 def qb_4(s4):  # 0 to 0.4068
-    c = -1 / Izz
-    boom1_s4 = (Ca - r + boomcoords_hinge[0, 0]) / math.cos(beta)
-    boomspace_s4 = -(boomcoords_hinge[0, 0] - boomcoords_hinge[1, 0]) / math.cos(beta)
+    c = -1 / sect.Izz
+    boom1_s4 = (sect.Ca - sect.r + sect.boomcoords_hinge[0, 0]) / math.cos(sect.beta)
+    boomspace_s4 = -(sect.boomcoords_hinge[0, 0] - sect.boomcoords_hinge[1, 0]) / math.cos(sect.beta)
     if s4 < boom1_s4:
-        qb = c * integrate(lambda x: -tskin * math.sin(beta) * x, 0, s4) + qb_3(0.4068)
+        qb = c * integrate(lambda x: -sect.tskin * math.sin(sect.beta) * x, 0, s4) + qb_3(0.4068)
     else:
         nbooms = int((s4 - boom1_s4) / boomspace_s4) + 1
         qb_booms = 0
         for i in range(0, 0 + nbooms):
-            qb_booms += stiff_area * boomcoords_hinge[i, 1]
-        qb_skin = integrate(lambda x: -tskin * math.sin(beta) * x, 0, s4)
+            qb_booms += sect.stiff_area * sect.boomcoords_hinge[i, 1]
+        qb_skin = integrate(lambda x: -sect.tskin * math.sin(sect.beta) * x, 0, s4)
         qb = c * (qb_skin + qb_booms) + qb_3(0.4068)
     return qb
 
 
 # SECTION 5
 def qb_5(s5):  # 0 to 0.0865
-    c = -1 / Izz
-    qb = c * integrate(lambda x: -tspar * x, 0, s5)
+    c = -1 / sect.Izz
+    qb = c * integrate(lambda x: -sect.tspar * x, 0, s5)
     return qb
 
 
 # SECTION 6
 def qb_6(theta):  # -pi/2 to 0
-    c = -1 / Izz
-    boom6_theta = math.asin(boomcoords[5, 1] / r)
+    c = -1 / sect.Izz
+    boom6_theta = math.asin(sect.boomcoords[5, 1] / sect.r)
     if theta < boom6_theta:
-        qb = c * (tskin * r ** 2) * integrate(math.sin, math.pi / 2, theta) + qb_5(0.0865) + qb_4(0.4068)
+        qb = c * (sect.tskin * sect.r ** 2) * integrate(math.sin, math.pi / 2, theta) + qb_5(0.0865) + qb_4(0.4068)
     else:
-        qb = c * ((tskin * r ** 2) * integrate(math.sin, -math.pi / 2, theta) + stiff_area * boomcoords[5, 1]) + qb_5(
+        qb = c * ((sect.tskin * sect.r ** 2) * integrate(math.sin, -math.pi / 2, theta) + sect.stiff_area * sect.boomcoords[5, 1]) + qb_5(
             0.0865) + qb_4(0.4068)
     return qb
 
@@ -94,22 +85,22 @@ def shear_centre(n):
     # SECTION 1 - base
     int_qb1 = 0
     for i in range(1, n + 1):
-        int_qb1 += (qb_1(i * (math.pi / (2 * n))) * ((math.pi * r) / (2 * n))) / tskin
+        int_qb1 += (qb_1(i * (math.pi / (2 * n))) * ((math.pi * sect.r) / (2 * n))) / sect.tskin
 
     # SECTION 2 - base (cell I)
     int_qb2I = 0
     for i in range(1, n + 1):
-        int_qb2I += (qb_2(r - i * (r / n)) * (r / n)) / tspar
+        int_qb2I += (qb_2(sect.r - i * (sect.r / n)) * (sect.r / n)) / sect.tspar
 
     # SECTION 5 - base (cell I)
     int_qb5I = 0
     for i in range(1, n + 1):
-        int_qb5I += (qb_5(i * (r / n)) * (r / n)) / tspar
+        int_qb5I += (qb_5(i * (sect.r / n)) * (sect.r / n)) / sect.tspar
 
     # SECTION 6 - base
     int_qb6 = 0
     for i in range(1, n + 1):
-        int_qb6 += (qb_6((-math.pi / 2) + i * (math.pi / (2 * n))) * ((math.pi * r) / (2 * n))) / tskin
+        int_qb6 += (qb_6((-math.pi / 2) + i * (math.pi / (2 * n))) * ((math.pi * sect.r) / (2 * n))) / sect.tskin
     print('Done computing cell I contribution.')
     qb_intI = int_qb1 - int_qb2I + int_qb5I + int_qb6
 
@@ -117,29 +108,29 @@ def shear_centre(n):
     # SECTION 2 - base (cell II)
     int_qb2II = 0
     for i in range(1, n + 1):
-        int_qb2II += (qb_2(i * (r / n)) * (r / n)) / tspar
+        int_qb2II += (qb_2(i * (sect.r / n)) * (sect.r / n)) / sect.tspar
 
     # SECTION 3 - base
     int_qb3 = 0
     for i in range(1, n + 1):
-        int_qb3 = (qb_3(i * (l_topskin / n)) * (l_topskin / n)) / tskin
+        int_qb3 = (qb_3(i * (sect.l_topskin / n)) * (sect.l_topskin / n)) / sect.tskin
 
     # SECTION 4 - base
     int_qb4 = 0
     for i in range(1, n + 1):
-        int_qb4 = (qb_4(i * (l_topskin / n)) * (l_topskin / n)) / tskin
+        int_qb4 = (qb_4(i * (sect.l_topskin / n)) * (sect.l_topskin / n)) / sect.tskin
 
     # SECTION 5 - base (cell II)
     int_qb5II = 0
     for i in range(1, n + 1):
-        int_qb5II += (qb_5(r - i * (r / n)) * (r / n)) / tspar
+        int_qb5II += (qb_5(sect.r - i * (sect.r / n)) * (sect.r / n)) / sect.tspar
     print('Done computing cell II contribution.')
     qb_intII = int_qb2II + int_qb3 + int_qb4 - int_qb5II
 
     # SYSTEM SOLVING
     eqs = np.array([
-        [(math.pi * r) / tskin + r / tspar, (-2 * r) / tspar],
-        [-2 * r / tspar, (2 * r / tspar) + (2 * l_topskin / tskin)]
+        [(math.pi * sect.r) / sect.tskin + sect.r / sect.tspar, (-2 * sect.r) / sect.tspar],
+        [-2 * sect.r / sect.tspar, (2 * sect.r / sect.tspar) + (2 * sect.l_topskin / sect.tskin)]
     ])
     cs = np.array([-qb_intI, -qb_intII])
     qsI, qsII = np.linalg.solve(eqs, cs)
@@ -174,26 +165,26 @@ def shear_centre(n):
     # SECTION 1
     int_q1 = 0
     for i in range(1, n + 1):
-        int_q1 += (q1(i * (math.pi / (2 * n))) * ((math.pi * r) / (2 * n)))
-    m1 = int_q1 * r
+        int_q1 += (q1(i * (math.pi / (2 * n))) * ((math.pi * sect.r) / (2 * n)))
+    m1 = int_q1 * sect.r
 
     # SECTION 3
     int_q3 = 0
     for i in range(1, n + 1):
-        int_q3 = (q3(i * (l_topskin / n)) * (l_topskin / n))
-    m3 = int_q3 * math.cos(beta) * r
+        int_q3 = (q3(i * (sect.l_topskin / n)) * (sect.l_topskin / n))
+    m3 = int_q3 * math.cos(sect.beta) * sect.r
 
     # SECTION 4
     int_q4 = 0
     for i in range(1, n + 1):
-        int_q4 = (q4(i * (l_topskin / n)) * (l_topskin / n))
-    m4 = int_q4 * math.cos(beta) * r
+        int_q4 = (q4(i * (sect.l_topskin / n)) * (sect.l_topskin / n))
+    m4 = int_q4 * math.cos(sect.beta) * sect.r
 
     # SECTION 6
     int_q6 = 0
     for i in range(1, n + 1):
-        int_q6 += (q6((-math.pi / 2) + i * (math.pi / (2 * n))) * ((math.pi * r) / (2 * n)))
-    m6 = int_q6 * r
+        int_q6 += (q6((-math.pi / 2) + i * (math.pi / (2 * n))) * ((math.pi * sect.r) / (2 * n)))
+    m6 = int_q6 * sect.r
 
     # FINAL COMPUTATION
     xi = -(m1 + m3 + m4 + m6)
@@ -202,3 +193,4 @@ def shear_centre(n):
 
 
 qsI, qsII, q1, q2, q3, q4, q5, q6, xi = shear_centre(1000)
+print(qsI, qsII, xi)

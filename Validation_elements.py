0c2a4d88b46c59c21ed8bc42356242e2d1910a5d
meesb <meesbeumer@hotmail.com>
2020-02-22 15:37:20 +0100

    Implemented MSE for all complete sections, data still to be delivered to section data array. If delivered it can be used

--- Validation_elements.py
+++ Validation_elements.py
@@ -13,44 +13,7 @@ def scatter3d(x,y,z, cs, colorsMap='jet'):
     scalarMap.set_array(cs)
     fig.colorbar(scalarMap)
     plt.show()
-'''
-def opentext(filename):
-    lijst = [filename, '.txt']
-    filename = ''.join(lijst)
-    document = open(filename, 'r')
-    text = document.read()
-    document.close()
-    return text
-def cut_text(text, cutsym):
-    listperline = text.split(cutsym)
-    arrayperline = np.array(listperline)
-    return arrayperline
-def plotxyz(array):
-    return
-text1 = opentext('data/nodes')
-nodeline = cut_text(text1, '\n')
-nodeline0 = nodeline[0]
-allnodes = []
-for i in range(len(nodeline)):
-    nodeline[i] = ''.join(nodeline[i].split())
-    nodelist = cut_text(nodeline[i], ',')
-    for i in range(len(nodelist)):
-        nodelist[i] = float(nodelist[i])
-    allnodes.append(nodelist)
-text1 = opentext('data/elements')
-nodeline = cut_text(text1, '\n')
-nodeline0 = nodeline[0]
-allelements = []
-for i in range(len(nodeline)):
-    nodeline[i] = ''.join(nodeline[i].split())
-    nodelist = cut_text(nodeline[i], ',')
-    for i in range(len(nodelist)):
-        nodelist[i] = float(nodelist[i])
-    allelements.append(nodelist)
-map(float, allelements)
-#print('allnodes', allnodes)
-#print('allelements', allelements)
-'''
+
 # starting to couple every element with 4 points.
 ######################################## loading data ##########################################
 path = 'data/nodes.txt'
@@ -72,20 +35,10 @@ file.close()
 data = np.zeros((np.size(elements),5))
 
 
-#data[:,0] = np.sum(nodes[np.where(elements[1])])/4
-
-#print(elements[0])
-#currentcoords = elements[0,1:]
-#print(currentcoords)
-#print(nodes[int(currentcoord)-1])
-
-#average_x = (nodes[int(currentcoords[0])-1,1] + nodes[int(currentcoords[1])-1,1] + nodes[int(currentcoords[2])-1,1] + nodes[int(currentcoords[3])-1,1])/4
-#print(average_x)
-
-#nested loop finds the averages (integration points) of all elements and stores them in data array
+# nested loop finds the averages (integration points) of all elements and stores them in data array
 # at the first 3 positions, the last 2 positions are reserved for the average stress and shear stress from
 # top to bottom
-#np.shape(elements
+
 ############################# Making data frame ##############################################
 for j in range(np.shape(elements)[0]):
 
@@ -117,10 +70,92 @@ for j in range(np.shape(elements)[0]):
 #scatter3d(data[0],data[1],data[2],data[4])
 
 
-
 ##################################### plotting  slices with unknown spacing ####################
-# usual number of nodes per section = 62
+
 next_section = np.unique(data[:,0])
+
+# select a cross section place to monitor the stresses
+
+discr_miss =[]
+discr_shear = []
+xloc = []
+for numb in range(np.shape(next_section)[0]):
+    section_data = np.zeros((62, 6))
+
+    #print(next_section[0:40])
+    # spacing of complete 24.6856995‬mm
+
+
+    # first check for section completeness (62 points)
+    i = 0
+    for j in range(np.shape(data)[0]):
+        if data[j,0] == next_section[numb]:
+            i += 1
+
+    # if section complete --> get out data for particular layer from the data array
+    if i == 62:
+        k = 0
+        for j in range(np.shape(data)[0]):
+            if data[j,0] == next_section[numb]:
+                section_data[k,0] = data[j,1]
+                section_data[k,1] = data[j,2]
+                section_data[k,2] = data[j,3]
+                section_data[k,3] = data[j,4]
+                k += 1
+
+
+        section_data =section_data.transpose()
+
+        #print(next_section[numb])
+        #plt.scatter(section_data[1],section_data[0],c=section_data[2])
+        #plt.colorbar()
+        #plt.show()
+
+
+    ############################# setup error testing ################################
+
+    # section data only populated by validation model, the two colums left to be filled by sampling the numerical model.
+
+
+    local_mse_miss = 1/62 * sum((section_data[2]-section_data[4]))**2
+    local_mse_shear = 1/62 * sum((section_data[3]-section_data[5]))**2
+
+    # store result in list
+    xloc.append(next_section[numb])
+    discr_miss.append(local_mse_miss)
+    discr_shear.append(local_mse_shear)
+
+
+# plot list of error
+
+plt.plot(xloc,discr_miss,xloc,discr_shear)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 '''
 # 3d plot the sections that are incomplete --> to check what is their physical
@@ -141,7 +176,7 @@ incomplete = np.concatenate(incomplete,axis=0)
 print(np.shape(incomplete))
 
 '''
-
+'''
 section_data = np.zeros((62,3))
 
 
@@ -203,40 +238,4 @@ plt.show()
 
 ############################################# working code to plot slicewise but with guesswork for spacing #########
 
-'''
-
-# untouched code to show layers, still manual
-
-section_data = np.zeros((62,3))
-
-# select a cross section place to monitor the stresses
-#print(next_section)
-numb = 40
-
-#print(next_section[0:40])
-# spacing of complete 24.6856995‬mm
-
-
-# first check for section completeness (62 points)
-i = 0
-for j in range(np.shape(data)[0]):
-    if data[j,0] == next_section[numb]:
-        i += 1
-
-# if section complete --> get out data for particular layer from the data array
-if i == 62:
-    k = 0
-    for j in range(np.shape(data)[0]):
-        if data[j,0] == next_section[numb]:
-            section_data[k,0] = data[j,1]
-            section_data[k,1] = data[j,2]
-            section_data[k,2] = data[j,3]
-            k += 1
-
-
-section_data =section_data.transpose()
-
-plt.scatter(section_data[1],section_data[0],c=section_data[2])
-plt.colorbar()
-plt.show()
 '''

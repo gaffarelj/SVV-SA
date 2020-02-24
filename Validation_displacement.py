4102c36c3cd4f6ec8aa18e9ba6aafb5f764f3a7a
meesb <meesbeumer@hotmail.com>
2020-02-24 16:15:37 +0100

    File that will evaluate the displacement of the hingeline

---
+++ Validation_displacement.py
@@ -0,0 +1,24 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+path = 'data/nodes.txt'
+file = open(path, "r")
+nodes = np.genfromtxt(path, delimiter=",", skip_header=0 )
+file.close()
+
+path = 'data/elements.txt'
+file = open(path, "r")
+elements = np.genfromtxt(path, delimiter=",", skip_header=0)
+file.close()
+
+path = 'data/Jamstraight_result_notext_nosmallvalues_dat2.csv'
+file = open(path, "r")
+data_frame = np.genfromtxt(path, delimiter=";", skip_header=0)
+file.close()
+
+path = 'data/Displ_jamstraight.csv'
+file = open(path, "r")
+displ_dat = np.genfromtxt(path, delimiter=",", skip_header=3)
+file.close()
+
+

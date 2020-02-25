01045599a84933ccd3682f0561599d7784aebf3d
jgaffarel <j.y.p.gaffarel@student.tudelft.nl>
2020-02-25 16:27:53 +0100

    Changed stress just a bit

--- main.py
+++ main.py
@@ -7,21 +7,22 @@ import stress as STR
 sect = SP.section(Ha=0.173, Ca=0.484, tskin=0.0011, tspar=0.0025, hstiff=0.014, tstiff=0.0012, wstiff=0.018)
 
 SC.set_sect(sect)
-#qsI, qsII, q1, q2, q3, q4, q5, q6, xi = SC.shear_centre(1000)
-xi = -0.007513567161803937
+qsI, qsII, q1, q2, q3, q4, q5, q6, xi = SC.shear_centre(1000)
+#xi = -0.007513567161803937
 
 _, _, J = TS.tosionalstiffness(sect)
 
-MC.set_vars(xi, J, sect.r, sect.Izz, sect.Iyy)
-Ry1, Ry2, Ry3, Rz1, Rz2, Rz3, Fa, C1, C2, C3, C4, C5 = MC.system()
-MC.do_plots()
-MC.plot_result(MC.My, "My_b")
+#MC.set_vars(xi, J, sect.r, sect.Izz, sect.Iyy)
+#Ry1, Ry2, Ry3, Rz1, Rz2, Rz3, Fa, C1, C2, C3, C4, C5 = MC.system()
+#MC.do_plots()
+#MC.plot_result(MC.My, "My_b")
 
 
 x = 0.554
-Mz, My = MC.Mz(x)[0], MC.My(x)[0]
-#Mz, My = -34645.28897198964, 119630.64930303364
+#Mz, My = MC.Mz(x)[0], MC.My(x)[0]
+Mz, My = -34645.28897198964, 119630.64930303364
 
-s = STR.stress(Mz, My, sect)
+s = STR.stress(Mz, My, sect, q1, q2, q3, q4, q5, q6)
 s.section_stress()
-s.plot_stress()
+#s.plot_stress()
+s.plot_shear_flows()
--- stress.py
+++ stress.py
@@ -3,46 +3,83 @@ from matplotlib import pyplot as plt
 import matplotlib as mpl
 
 class stress():
-	def __init__(self, Mz, My, sect):
+	def __init__(self, Mz, My, sect, q1, q2, q3, q4, q5, q6):
 		self.Iyy = sect.Iyy
 		self.Izz = sect.Izz
 		self.Mz = Mz
 		self.My = My
 		self.stresses = []
+		self.shear_flows = []
 		self.r = sect.r
 		self.beta = sect.beta
 		self.l_topskin = sect.l_topskin
+		self.l_spar_to_end = sect.l_spar_to_end
 		self.zc = sect.z_centroid
+		self.q1, self.q2, self.q3, self.q4, self.q5, self.q6 = q1, q2, q3, q4, q5, q6
 
 	def section_stress(self):
 		# Hinge
 		z = 0
 		ys = np.arange(-self.r, self.r, 0.0025)
 		for y in ys:
-			self.add_res(z, y)
+			self.add_stress(z, y)
+			if y > 0:
+				s = y
+				self.add_shear(z, y, s, self.q2)
+			else:
+				s = -y
+				self.add_shear(z, y, s, self.q5)
 		# Circle
 		for theta in np.arange(0, np.pi, 0.025):
 			z = np.sin(theta) * self.r
-			y = np.cos(theta) * self.r
-			self.add_res(z, y)
+			y = -np.cos(theta) * self.r
+			self.add_stress(z, y)
+			s = theta - np.pi/2
+			if theta < np.pi/2:
+				self.add_shear(z, y, s, self.q6)
+			else:
+				self.add_shear(z, y, s, self.q1)
 		# Top sheet
-		for p in np.arange(0, self.l_topskin, 0.005):
-			z = -p * np.cos(self.beta)
-			y = self.r - p * np.sin(self.beta)
-			self.add_res(z, y)
+		for s in np.arange(0, self.l_topskin, 0.005):
+			z = -s * np.cos(self.beta)
+			y = self.r - s * np.sin(self.beta)
+			self.add_stress(z, y)
+			self.add_shear(z, y, s, self.q3)
 		# Bottom sheet
-		for p in np.arange(0, self.l_topskin, 0.005):
-			z = -p * np.cos(self.beta)
-			y = -self.r + p * np.sin(self.beta)
-			self.add_res(z, y)
+		for s in np.arange(0, self.l_topskin, 0.005):
+			z = -s * np.cos(self.beta)
+			y = -self.r + s * np.sin(self.beta)
+			self.add_stress(z, y)
+			self.add_shear(-z - self.l_spar_to_end, -y - self.r, s, self.q4)
 
-	def add_res(self, z, y):
+	def add_stress(self, z, y):
 		z -= self.zc
 		strs = self.stress_norm_xx(y, z)
 		self.stresses.append([z, y, strs])
 
+	def add_shear(self, z, y, s, q):
+		z -= self.zc
+		shear = q(s)
+		self.shear_flows.append([z, y, shear])
+
 	def stress_norm_xx(self, y, z):
 		return self.Mz / self.Izz * y + self.My / self.Iyy * z
+	
+	def plot_everything(self):
+		data = np.array(self.stresses)
+		z, y, s = data[:,0], data[:,1], data[:,2]
+		plt.xlim(0.225, -0.325)
+		plt.ylim(-0.09, 0.09)
+		plt.xlabel("z [m]")
+		plt.ylabel("y [m]")
+		plt.scatter(z, y, c=s)
+		plt.colorbar()
+		plt.set_cmap("jet")
+		#z_line = np.arange(-0.2, 0.2, 0.001)
+		#alpha = np.arctan(-self.My*self.Izz/self.Mz/self.Iyy)
+		#y_line = [z_c * np.sin(alpha) for z_c in z_line]
+		#plt.plot(z_line, y_line)
+		plt.show()	
 
 	def plot_stress(self):
 		data = np.array(self.stresses)
@@ -54,8 +91,20 @@ class stress():
 		plt.scatter(z, y, c=s)
 		plt.colorbar()
 		plt.set_cmap("jet")
-		z_line = np.arange(-0.2, 0.2, 0.001)
-		alpha = np.arctan(-self.My*self.Izz/self.Mz/self.Iyy)
-		y_line = [z_c * np.sin(alpha) for z_c in z_line]
-		plt.plot(z_line, y_line)
+		#z_line = np.arange(-0.2, 0.2, 0.001)
+		#alpha = np.arctan(-self.My*self.Izz/self.Mz/self.Iyy)
+		#y_line = [z_c * np.sin(alpha) for z_c in z_line]
+		#plt.plot(z_line, y_line)
+		plt.show()
+			
+	def plot_shear_flows(self):
+		data = np.array(self.shear_flows)
+		z, y, s = data[:,0], data[:,1], data[:,2]
+		plt.xlim(0.225, -0.325)
+		plt.ylim(-0.09, 0.09)
+		plt.xlabel("z [m]")
+		plt.ylabel("y [m]")
+		plt.scatter(z, y, c=s)
+		plt.colorbar()
+		plt.set_cmap("jet")
 		plt.show()

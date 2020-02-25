6fbc8190e01d3384b90d86cd147a72e680201308
shineminan <m.shi@student.tudelft.nl>
2020-02-25 12:31:24 +0100

    torsional stiffness calculated

--- torsionstiffness.py
+++ torsionstiffness.py
@@ -11,9 +11,9 @@ section_prop = SP.section(Ha=0.173, Ca=0.484, tskin=0.0011, tspar=0.0025, hstiff
 SC.set_sect(section_prop)
 qsI, qsII, q1, q2, q3, q4, q5, q6, xi = SC.shear_centre(1000)
 print("shear center:", xi)
-
-print(q1(0.03),"hello")
-print(q5(0.03))
+#
+# print(q1(section_prop.Ha/2),"hello")
+# print(q5(section_prop.Ha/2))
 
 def torsionstiffness():
 	"""
@@ -29,22 +29,31 @@ def torsionstiffness():
 	tspar = section_prop.tspar
 	tskin = section_prop.tskin
 	a1 = math.pi*ha**2/8  # enclosed area
+	a2 = ha*(1.691-r)/2
 
 	n = 1000
 	int_q1 = 0
-	int_q2 = 0#(q2(0) + q2(ha / 2)) * ha / 4 / tspar
-	int_q5 = 0#(q5(0) + q5(ha / 2)) * ha / 4 / tspar
+	int_q2 = 0
+	int_q5 = 0
 	int_q6 = 0
+
+	# int_q3 = 0
+	# int_q4 = 0
+	l = math.sqrt((ha/2)**2+(1.691-ha/2)**2)
 	for i in range(n+1):
 		int_q1 += q1(i * np.pi / (2 * n)) * np.pi * r / (2 * n) / tskin
-		int_q6 += q6(i * np.pi / (2 * n)) * np.pi * r / (2 * n) / tskin
 		int_q2 += q2(r - i * r / n) * r / n / tspar
 		int_q5 += q5(r - i * r / n) * r / n / tspar
-	print(int_q2,(q2(0) + q2(ha / 2)) * ha / 4 / tspar)
-	print(int_q5,(q5(0) + q5(ha / 2)) * ha / 4 / tspar)
+		int_q6 += q6(i * np.pi / (2 * n)) * np.pi * r / (2 * n) / tskin
+
+		# int_q3 += q3(i * l / n) * l / n / tskin
+		# int_q4 += q4(i * l / n) * l / n / tskin
 
 	G_dtheta_dz = (int_q1+int_q2+int_q5+int_q6)/2/a1
 	J = T/G_dtheta_dz
+
+	# g = (int_q2+int_q3+int_q4+int_q5)/2/a2
+	# J2 = T/g
 	return J
 
 a = torsionstiffness()
--- vonmisesstress.py
+++ vonmisesstress.py
@@ -1,14 +1,24 @@
-import math
+import sectionproperties as SP
+import shearcentre as SC
+import torsionstiffness as TS
+import macaulay_new as MA
 
-def vmstress(shearstress,mz,my):
+section_prop = SP.section(Ha=0.173, Ca=0.484, tskin=0.0011, tspar=0.0025, hstiff=0.014, tstiff=0.0012, wstiff=0.018)
+# The following parameters can be used out of section_prop (example: print(section_prop.z_centroid)
+# Iyy, Izz, Am, z_centroid, stiff_area, boomcoords, boomcoords_hinge,
+# tskin, tspar, hstiff, tstiff, wstiff, Ha, Ca, r, l_topskin, beta, l_spar_to_end
+SC.set_sect(section_prop)
+qsI, qsII, q1, q2, q3, q4, q5, q6, xi = SC.shear_centre(1000)
+print("shear center:", xi)
+
+x = 0.5
+def vmstress(x=0.5,y,z):
 	"""
 	Calculate the Von Mises stress with formula which can be simplified as:
 	sigma_vm = sqrt(sigma_xx^2 + 3*tau_yz^2)
 	Inputs:
-	- shearstress:
-	- mz:
-	- my:
-	"""
 
+	"""
+	sigma =MA.My(x)/section_prop.Iyy*z + 
 	vm = math.sqrt(sigma_xx**2+3*tau_yz**2)
 	return vm

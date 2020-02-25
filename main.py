98474279f1505ed83c3a7dc27a785782cf5b179c
shineminan <m.shi@student.tudelft.nl>
2020-02-25 13:38:45 +0100

    torsional stiffness done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sectionproperties as SP
 import shearcentre as SC
 import macaulay as MC
+import  torsionstiffness as TS
 
 section_prop = SP.section(Ha=0.173, Ca=0.484, tskin=0.0011, tspar=0.0025, hstiff=0.014, tstiff=0.0012, wstiff=0.018)
 # The following parameters can be used out of section_prop (example: print(section_prop.z_centroid)
@@ -10,9 +11,12 @@ section_prop = SP.section(Ha=0.173, Ca=0.484, tskin=0.0011, tspar=0.0025, hstiff
 SC.set_sect(section_prop)
 qsI, qsII, q1, q2, q3, q4, q5, q6, xi = SC.shear_centre(1000)
 
-J = 8.629971582027014e-06
+_, _, J = TS.tosionalstiffness()
+# J = 8.629971582027014e-06
 
 MC.set_vars(xi, J, section_prop.r, section_prop.Izz, section_prop.Iyy)
 Ry1, Ry2, Ry3, Rz1, Rz2, Rz3, Fa, C1, C2, C3, C4, C5 = MC.system()
 MC.do_plots()
 MC.plot_result(MC.My, "My_b")
+
+
--- torsionstiffness.py
+++ torsionstiffness.py
@@ -11,24 +11,23 @@ section_prop = SP.section(Ha=0.173, Ca=0.484, tskin=0.0011, tspar=0.0025, hstiff
 SC.set_sect(section_prop)
 
 def tosionalstiffness():
-	T = 1
-	r = section_prop.Ha / 2
-	ha = section_prop.Ha
+	T = 1  # unit force
+	r = section_prop.Ha / 2  # radius of semi circle
+	ha = section_prop.Ha  # height of the aileron
 	tspar = section_prop.tspar
 	tskin = section_prop.tskin
-	l = math.sqrt((ha / 2) ** 2 + (section_prop.Ca - ha / 2) ** 2)
-	a1 = math.pi * ha ** 2 / 8
+	l = math.sqrt((ha / 2) ** 2 + (section_prop.Ca - ha / 2) ** 2)  # length of the skin in triangular part
+	a1 = math.pi * ha ** 2 / 8  # enclosed areas
 	a2 = ha * (section_prop.Ca - r)/2
 
-	A = np.zeros((3,3))
-	b = np.zeros((3,1))
-	A[0, :] = [2*a1, 2*a2, 0]
-	A[1, :] = [(r*math.pi/tskin + 2*r/tspar) / (2*a1), (-2*r/tspar)/(2*a1), -1]
-	A[2, :] = [(-2*r/tspar)/(2*a2), (2*l/tskin + 2 * r/tspar)/(2*a2),-1]
+	A = np.zeros((3, 3))
+	b = np.zeros((3, 1))
+	A[0, :] = [2*a1, 2*a2, 0]  # T = T1 + T2 = 1
 	b[0] = 1
+	A[1, :] = [(r*math.pi/tskin + 2*r/tspar) / (2*a1), (-2*r/tspar)/(2*a1), -1]  # redundant shear flow in cell 1
+	A[2, :] = [(-2*r/tspar)/(2*a2), (2*l/tskin + 2 * r/tspar)/(2*a2), -1]  # redundant shear flow in cell 2
+
 	result = np.linalg.solve(A,b)
-	print(result)
-	J = 1/result[-1,0]
-	return J
-print(tosionalstiffness())
+	J = 1/result[-1, 0]
+	return result[0], result[1], J
 

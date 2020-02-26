8bdc5cc2423e65508bfab5f19b2e144bf746b441
meesb <meesbeumer@hotmail.com>
2020-02-26 16:50:28 +0100

    Moved some displacement data

--- Validation_displacement.py
+++ Validation_displacement.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import sys
 import os.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 import sectionproperties as SP
 import shearcentre as SC
 import macaulay as MC
